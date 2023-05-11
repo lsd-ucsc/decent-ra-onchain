3337837bbb7a2b04038d7588a60ff90c647a927d
Haofan Zheng <hzheng6@ucsc.edu>
2023-05-11 16:05:42 -0700

    Added github action for unit tests

--- tests/Decent/01_ParamPassing_test.sol
+++ tests/Decent/01_ParamPassing_test.sol
@@ -36,69 +36,15 @@ contract ParamPassing_testSuite {
         Assert.equal(retVal, 3, "retVal must be 3");
     }
 
-    function libGasDiff() public {
-        bytes memory b = new bytes(4096);
-        b[0] = 0x01;
-
-        // library
-
-        uint256 internMemGasUsed = gasleft();
-        uint256 retVal = LibParamPassing.internMemFunc(b);
-        internMemGasUsed -= gasleft();
+    /**
+     * library + private ==> Pass by reference (not visible)
+     */
+    function libPrivMemFunc() public {
+        LibParamPassing.Struct1 memory s;
+        uint256 retVal = LibParamPassing.privMemFuncProxy(s);
+        Assert.equal(s.a, 1, "s.a must be 1");
+        Assert.equal(s.b, 2, "s.b must be 2");
         Assert.equal(retVal, 3, "retVal must be 3");
-
-        uint256 externMemGasUsed = gasleft();
-        retVal = LibParamPassing.externMemFunc(b);
-        externMemGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-
-        uint256 externCallGasUsed = gasleft();
-        retVal = LibParamPassing.externCallFunc(b);
-        externCallGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-
-        Assert.lesserThan(internMemGasUsed, uint256(500), "internMemGasUsed");
-        // Assert.equal(internMemGasUsed, 152, "internMemGasUsed");
-        Assert.greaterThan(externMemGasUsed, uint256(5000), "externMemGasUsed");
-        // Assert.equal(externMemGasUsed, 15145, "externMemGasUsed");
-        Assert.greaterThan(externCallGasUsed, uint256(5000), "externCallGasUsed");
-        // Assert.equal(externCallGasUsed, 11264, "externCallGasUsed");
-
-        // contract
-        ContraParamPassing c = new ContraParamPassing();
-
-        externMemGasUsed = gasleft();
-        retVal = c.externMemFunc(b);
-        externMemGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-
-        externCallGasUsed = gasleft();
-        retVal = c.externCallFunc(b);
-        externCallGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-
-        uint256 pubCallGasUsed = gasleft();
-        retVal = c.pubCallFunc(b);
-        pubCallGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-
-        Assert.greaterThan(externMemGasUsed, uint256(5000), "externMemGasUsed");
-        // Assert.equal(externMemGasUsed, 12207, "externMemGasUsed");
-        Assert.greaterThan(externCallGasUsed, uint256(5000), "externCallGasUsed");
-        // Assert.equal(externCallGasUsed, 11298, "externCallGasUsed");
-        Assert.greaterThan(pubCallGasUsed, uint256(5000), "pubCallGasUsed");
-        // Assert.equal(pubCallGasUsed, 11277, "pubCallGasUsed");
-
-        pubCallGasUsed = gasleft();
-        retVal = c.pubCallFunc(m_b);
-        pubCallGasUsed -= gasleft();
-        Assert.equal(retVal, 3, "retVal must be 3");
-        Assert.greaterThan(pubCallGasUsed, uint256(50000), "pubCallGasUsed");
-        // Assert.equal(pubCallGasUsed, 281951, "pubCallGasUsed");
-
-        uint256 internCallGasUsed = c.forwardInternCallFunc(b);
-        Assert.lesserThan(internCallGasUsed, uint256(500), "internCallGasUsed");
-        Assert.equal(internCallGasUsed, 166, "internCallGasUsed");
     }
 
     /**
@@ -122,17 +68,6 @@ contract ParamPassing_testSuite {
         Assert.equal(s.b, 0, "s.b must be 0");
         Assert.equal(retVal, 3, "retVal must be 3");
     }
-
-    /**
-     * library + private ==> not visible
-     */
-    // function libPrivMemFunc() public {
-    //     LibParamPassing.Struct1 memory s;
-    //     uint256 retVal = LibParamPassing.privMemFunc(s);
-    //     Assert.equal(s.a, 0, "s.a must be 0");
-    //     Assert.equal(s.b, 0, "s.b must be 0");
-    //     Assert.equal(retVal, 3, "retVal must be 3");
-    // }
 
     /**
      * contract + external ==> Pass by copy
@@ -185,5 +120,77 @@ contract ParamPassing_testSuite {
     //     Assert.equal(s.b, 0, "s.b must be 0");
     //     Assert.equal(retVal, 3, "retVal must be 3");
     // }
+
+    function libGasDiff() public {
+        bytes memory b = new bytes(4096);
+        b[0] = 0x01;
+
+        // library
+
+        uint256 internMemGasUsed = gasleft();
+        uint256 retVal = LibParamPassing.internMemFunc(b);
+        internMemGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        uint256 privMemGasUsed = gasleft();
+        retVal = LibParamPassing.privMemFuncProxy(b);
+        privMemGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        uint256 externMemGasUsed = gasleft();
+        retVal = LibParamPassing.externMemFunc(b);
+        externMemGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        uint256 externCallGasUsed = gasleft();
+        retVal = LibParamPassing.externCallFunc(b);
+        externCallGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        Assert.lesserThan(internMemGasUsed, uint256(500), "internMemGasUsed");
+        // Assert.equal(internMemGasUsed, 152, "internMemGasUsed");
+        Assert.lesserThan(privMemGasUsed, uint256(500), "privMemGasUsed");
+        // Assert.equal(privMemGasUsed, 189, "privMemGasUsed");
+        Assert.greaterThan(externMemGasUsed, uint256(5000), "externMemGasUsed");
+        // Assert.equal(externMemGasUsed, 15145, "externMemGasUsed");
+        Assert.greaterThan(externCallGasUsed, uint256(5000), "externCallGasUsed");
+        // Assert.equal(externCallGasUsed, 11264, "externCallGasUsed");
+
+        // contract
+        ContraParamPassing c = new ContraParamPassing();
+
+        externMemGasUsed = gasleft();
+        retVal = c.externMemFunc(b);
+        externMemGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        externCallGasUsed = gasleft();
+        retVal = c.externCallFunc(b);
+        externCallGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        uint256 pubCallGasUsed = gasleft();
+        retVal = c.pubCallFunc(b);
+        pubCallGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+
+        Assert.greaterThan(externMemGasUsed, uint256(5000), "externMemGasUsed");
+        // Assert.equal(externMemGasUsed, 12207, "externMemGasUsed");
+        Assert.greaterThan(externCallGasUsed, uint256(5000), "externCallGasUsed");
+        // Assert.equal(externCallGasUsed, 11298, "externCallGasUsed");
+        Assert.greaterThan(pubCallGasUsed, uint256(5000), "pubCallGasUsed");
+        // Assert.equal(pubCallGasUsed, 11277, "pubCallGasUsed");
+
+        pubCallGasUsed = gasleft();
+        retVal = c.pubCallFunc(m_b);
+        pubCallGasUsed -= gasleft();
+        Assert.equal(retVal, 3, "retVal must be 3");
+        Assert.greaterThan(pubCallGasUsed, uint256(50000), "pubCallGasUsed");
+        // Assert.equal(pubCallGasUsed, 281951, "pubCallGasUsed");
+
+        uint256 internCallGasUsed = c.forwardInternCallFunc(b);
+        Assert.lesserThan(internCallGasUsed, uint256(500), "internCallGasUsed");
+        // Assert.equal(internCallGasUsed, 166, "internCallGasUsed");
+    }
 
 }
